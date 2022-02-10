1d72422daa463fb8192ec96a43514229965c5d26
Mudia Arha <mudiageo@gmail.com>
2022-02-10 21:11:48 +0100

    Add posts index page and slug page

--- src/lib/graphql-queries.js
+++ src/lib/graphql-queries.js
@@ -41,3 +41,36 @@ query GetProject($slug: String) {
   }
 }
 `
+const POST_FRAGMENT = gql`
+  fragment PostDetails on Post {
+    title
+    slug
+    date
+    content
+    tags
+    coverImage {
+      url
+    }
+    authors {
+      name
+    }
+  }
+`
+
+export const postsQuery = gql`
+  ${POST_FRAGMENT}
+  query GetPosts {
+    posts {
+      ...PostDetails
+    }
+  }
+`
+
+export const postQuery = gql`
+  ${POST_FRAGMENT}
+  query GetPost($slug: String!) {
+    post(where: { slug: $slug }) {
+      ...PostDetails
+    }
+  }
+`
