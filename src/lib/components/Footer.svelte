2316180fc0eaf25fcb0660de3ea92e75b56a4120
Mudia Arha <mudiageo@gmail.com>
2022-02-10 21:21:49 +0100

    Add footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,34 @@
+<script>
+    import FacebookIcon from './FacebookIcon.svelte'
+    import TwitterIcon from './TwitterIcon.svelte'
+    import YouTubeIcon from './YoutubeIcon.svelte'
+  </script>
+  
+  <footer
+    class="p-10 footer bg-primary text-primary-content footer-center"
+  >
+    <div class="grid grid-flow-col gap-4">
+      <a class="link link-hover" href="/projects">Portfolio</a>
+      <a class="link link-hover" href="/posts">Blog</a>
+      <a class="link link-hover" href="/about">About</a>
+    </div>
+    <div>
+      <div class="grid grid-flow-col gap-4">
+        <a href="https://twitter.com">
+          <TwitterIcon />
+        </a>
+        <a href="https://youtube.com">
+          <YouTubeIcon />
+        </a>
+        <a href="https://facebook.com">
+          <FacebookIcon />
+        </a>
+      </div>
+    </div>
+    <div>
+      <p>
+        Copyright &copy; {`${new Date().getFullYear()}`} - All right reserved
+        by ME
+      </p>
+    </div>
+  </footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,9 @@
 <script>
+    import Footer from '$lib/components/Footer.svelte'
     import '../app.css'
-</script>
-<main class="container max-w-3xl mx-auto px-4 mb-20">
+  </script>
+  
+  <main class="container max-w-3xl mx-auto px-4 mb-20">
     <slot />
-</main>
+  </main>
+  <Footer />
