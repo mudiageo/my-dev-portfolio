b65321365f06c00ab11cd71111509e56aaf24ecf
Mudia Arha <mudiageo@gmail.com>
2022-02-10 22:12:13 +0100

    Add Tailwind Typography, daisyui and marked

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,19 @@
 <script>
-    import Footer from '$lib/components/Footer.svelte'
-    import Navbar from '$lib/components/Navbar.svelte'
-    import { onMount } from 'svelte'
-    import { themeChange } from 'theme-change'
-    import '../app.css'
-  
-    onMount(async () => {
-      themeChange(false)
-    })
-  </script>
-  
-  <Navbar />
-  <main class="container max-w-3xl mx-auto px-4 mb-20">
-    <slot />
-  </main>
-  <Footer />
+	import Footer from '$lib/components/Footer.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+	import '../app.css';
+
+	onMount(async () => {
+		themeChange(false);
+	});
+</script>
+
+<Navbar>
+	<main class="container max-w-3xl mx-auto px-4 mb-20">
+		<slot />
+	</main>
+	<Footer /></Navbar
+>
+<slot />
