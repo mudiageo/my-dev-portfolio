79d472a7623724b18fb87c6798ca49272151037f
Mudia Arha <mudiageo@gmail.com>
2022-02-10 22:17:35 +0100

    Add plugins to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,21 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			typography:{
+				DEFAULT:{
+					css: {
+						maxWidth: null,
+					}
+				} 
+			}
+		}
 	},
 
-	plugins: []
+	plugins: [ 
+		require('@tailwindcss/typography'),
+		require('daisyui'),
+	]
 };
 
 module.exports = config;
