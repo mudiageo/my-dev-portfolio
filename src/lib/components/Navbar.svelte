fe50b25b3f3223e38806ee649600045c9c748b14
Mudia Arha <mudiageo@gmail.com>
2022-02-13 00:00:47 +0100

    Update to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -10,92 +10,87 @@
   })
 })
 </script>
-<div
-  class="navbar mb-16 shadow-lg bg-neutral text-neutral-content sticky top-0 z-10"
->
-<nav class="bg-white shadow-lg">
+<nav class="mb-16 shadow-lg bg-neutral text-neutral-content sticky top-0 z-10">
   <div class="max-w-6xl mx-auto px-4">
       <div class="flex justify-between">
           <div class="flex space-x-7">
               <!--Website Logo-->
-              <a href="/" class="flex items-center py-4 px-2">
-<img src="favicon.png" alt="Logo" class="h-8 w-8 mr-2">
-                  <span class="font-semibold text-gray-500 text-lg">
+              <a sveltekit:prefetch href="/" class="flex items-center py-4 px-2">
+              <img src="favicon.png" alt="Logo" class="h-8 w-8 mr-2">
+                  <span class="font-semibold text-lg">
                       Mudiaga Arharhire
                   </span>
               </a>
           </div>
-          <div class="hidden md:flex items-center space-x-1">
-           <a href="/" 
-class="py-4 px-2 text-gray-500 border-b-4 border-purple-500 font-semibold">
+  <div class="hidden md:flex items-center space-x-1">
+<a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/" class="py-4 px-2 border-b-4 border-purple-500 font-semibold">
  Home
-</a>
-<a href="/"
-    class="py-4 px-2 text-gray-500 font-semibold hover:text-purple-500 transition duration-300">
+           </a>
+<a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/" class="py-4 px-2 font-semibold hover:text-purple-500 transition duration-300">
  Services
-</a>
-    
-<a href="/portfolio"
-class="py-4 px-2 text-gray-500 font-semibold hover:text-purple-500 transition duration-300">
+</a>  
+<a class:active={$page.url.pathname === '/projects'} sveltekit:prefetch href="/projects" class="py-4 px-2 font-semibold hover:text-purple-500 transition duration-300">
 Portfolio
 </a>
 
-<a href="/posts"
-class="py-4 px-2 text-gray-500 font-semibold hover:text-purple-500 transition duration-300">
+<a class:active={$page.url.pathname === '/posts'} sveltekit:prefetch href="/posts"
+class="py-4 px-2 font-semibold hover:text-purple-500 transition duration-300">
 Blog
 </a>
-<a href="/about"
-          class="py-4 px-2 text-gray-500 font-semibold hover:text-purple-500 transition duration-300">
+<a class:active={$page.url.pathname === '/about'} sveltekit:prefetch href="/about" class="py-4 px-2 font-semibold hover:text-purple-500 transition duration-300">
        About
  </a>
           <a href="/"
           class="py-4 px-2 text-gray-500 font-semibold hover:text-purple-500 transition duration-300">
        Contact Us
 </a>
-           </div>
+</div>
 <div class="md:hidden flex items-center">
 <button class="outline-none menu-button">
-           <svg class="w-6 h-6 text-gray-500" x-show="! showMenu" fill="none" stroke-linecap="round"
-               stroke-linejoin="round" stroke-width="2" viewBox="0 00 24 24" stroke="currentColor"><path d="m4 6h16M4 12h16M4 18h16"></path>
-</svg>
-       </button>
+  <svg class="w-6 h-6 " x-show="! showMenu" fill="none" stroke-linecap="round"
+                stroke-linejoin="round" stroke-width="2" viewBox="0 00 24 24" stroke="currentColor"><path d="m4 6h16M4 12h16M4 18h16"></path>
+  </svg>
+</button>
    </div>
    <div class="hidden mobile-menu">
        <ul class="">
-           <li class="active"> 
-             <a href="/"
-              class="block text-sm px-2 py-4 text-white bg-purple-500 font-semibold">
+           <li> 
+             <a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/"
+              class="block text-sm px-2 py-4 bg-purple-500 font-semibold">
              Home
            </li>
            <li>
-            <a href="/"
+            <a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/"
              class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
             Services
           </a>
           </li>
           <li>
-            <a href="/projects"
+            <a class:active={$page.url.pathname === '/projects'} sveltekit:prefetch href="/projects"
              class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
             Portfolio
           </a>
           </li>
           <li>
-            <a href="/posts"
+            <a class:active={$page.url.pathname === '/posts'} sveltekit:prefetch href="/posts"
              class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
             Blog
           </a>
           </li>
            <li>
-           <a href="/about"
+           <a class:active={$page.url.pathname === '/about'} sveltekit:prefetch href="/about"
             class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
            About
          </a>
            </li>
            <li>
-            <a href="/"
+            <a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/"
              class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
            Contact Us
      </a></li>
+     <li class="px-4">
+      <ThemeSelect />
+    </li>
        </ul>
    </div>
 </div>
@@ -139,4 +134,3 @@ Blog
       >
     </div>
   </div> -->
-</div>
