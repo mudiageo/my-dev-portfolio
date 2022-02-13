fcf679b2ac71311101d3f6285d04d0ecae24eaf4
Mudia Arha <mudiageo@gmail.com>
2022-02-13 21:57:35 +0100

    Updated new service worker

--- src/service-worker/fetchEvent.ts
+++ src/service-worker/fetchEvent.ts
@@ -1,15 +1,47 @@
 import { CACHE_NAME } from './constants';
-import { build } from '$service-worker';
 
-export default (event: ExtendableEvent): void => {
-   console.log('installing service worker');
+export default (event: FetchEvent): void => {
+   event.respondWith(
+      caches.match(event.request).then((cacheResponse) => {
 
-   event.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-         // Open a cache and cache our files
-         cache.addAll(build);
+         if (cacheResponse) {
+            console.info(`fetching from cache: ${event.request.url}`);
 
-         return true;
+            return cacheResponse;
+         }
+
+         console.info(`trying to fetch from server: ${event.request.url}`);
+
+         return fetch(event.request)
+            .then(async (fetchResponse): Promise<Response | undefined> => {
+               if (
+                  event.request.url.indexOf('http') !== -1
+               ) {
+                  const cache = await caches.open(CACHE_NAME);
+
+                  try {
+                     // filter what to add to the cache
+                     if (
+                        fetchResponse.status !== 206
+                     ) {
+                        cache.put(event.request.url, fetchResponse.clone());
+                     }
+
+                  } catch (error) {
+                     console.error(error);
+                  }
+
+                  return fetchResponse;
+               }
+
+               // eslint-disable-next-line consistent-return
+               return undefined;
+            })
+            .catch(((error) => {
+               console.error(`"${error}: ${event.request.url}`);
+
+               return error;
+            }));
       }),
    );
 };
