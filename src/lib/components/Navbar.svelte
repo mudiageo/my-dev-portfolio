56caf46a3c2e6a82179562dd237ba959d26af8ef
Mudia Arha <mudiageo@gmail.com>
2022-02-13 19:32:39 +0100

    Update Navbar.svelte

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -17,7 +17,7 @@ w-full
 py-4
 md:py-0
 px-4
-text-lgmb-16 shadow-lg bg-neutral text-neutral-content sticky top-0 z-10">
+text-lg mb-16 shadow-lg bg-neutral text-neutral-content sticky top-0 z-10">
 
           <div>
               <!--Website Logo-->
@@ -28,78 +28,69 @@ text-lgmb-16 shadow-lg bg-neutral text-neutral-content sticky top-0 z-10">
                   </span>
               </a>
           </div>
-  <div class="hidden md:flex items-center space-x-1">
-<a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/" class="py-4 px-2 border-b-4 border-purple-500 font-semibold">
- Home
-           </a>
-<a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/" class="py-4 px-2 font-semibold hover:text-purple-500 transition duration-300">
- Services
-</a>  
-<a class:active={$page.url.pathname === '/projects'} sveltekit:prefetch href="/projects" class="py-4 px-2 font-semibold hover:text-purple-500 transition duration-300">
-Portfolio
-</a>
+          <svg
+          xmlns="http://www.w3.org/2000/svg"
+          id="menu-button"
+          class="h-6 w-6 cursor-pointer md:hidden block"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+     
 
-<a class:active={$page.url.pathname === '/posts'} sveltekit:prefetch href="/posts"
-class="py-4 px-2 font-semibold hover:text-purple-500 transition duration-300">
-Blog
-</a>
-<a class:active={$page.url.pathname === '/about'} sveltekit:prefetch href="/about" class="py-4 px-2 font-semibold hover:text-purple-500 transition duration-300">
-       About
- </a>
-          <a href="/"
-          class="py-4 px-2 text-gray-500 font-semibold hover:text-purple-500 transition duration-300">
-       Contact Us
-</a>
-</div>
-<div class="md:hidden flex items-center">
-<button class="outline-none menu-button">
-  <svg class="w-6 h-6 " x-show="! showMenu" fill="none" stroke-linecap="round"
-                stroke-linejoin="round" stroke-width="2" viewBox="0 00 24 24" stroke="currentColor"><path d="m4 6h16M4 12h16M4 18h16"></path>
-  </svg>
-</button>
-   </div>
-   <div class="hidden mobile-menu">
-       <ul class="">
+  
+   <div class="hidden w-full md:flex md:items-center md:w-auto" id="menu">
+       <ul class=" pt-4
+       text-base 
+       md:flex
+       md:justify-between 
+       md:pt-0">
            <li> 
              <a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/"
-              class="block text-sm px-2 py-4 bg-purple-500 font-semibold">
+              class="block text-sm px-2 py-4 font-semibold">
              Home
            </li>
            <li>
             <a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/"
-             class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
+             class="block text-sm px-2 py-4 transition duration-300">
             Services
           </a>
           </li>
           <li>
             <a class:active={$page.url.pathname === '/projects'} sveltekit:prefetch href="/projects"
-             class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
+             class="block text-sm px-2 py-4 transition duration-300">
             Portfolio
           </a>
           </li>
           <li>
             <a class:active={$page.url.pathname === '/posts'} sveltekit:prefetch href="/posts"
-             class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
+             class="block text-sm px-2 py-4 transition duration-300">
             Blog
           </a>
           </li>
            <li>
            <a class:active={$page.url.pathname === '/about'} sveltekit:prefetch href="/about"
-            class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
+            class="block text-sm px-2 py-4 transition duration-300">
            About
          </a>
            </li>
            <li>
             <a class:active={$page.url.pathname === '/'} sveltekit:prefetch href="/"
-             class="block text-sm px-2 py-4 hover:bg-purple-500 transition duration-300">
+             class="block text-sm px-2 py-4 transition duration-300">
            Contact Us
      </a></li>
-     <li class="px-4">
+     <li class="block text-sm px-2 py-4 transition duration-300">
       <ThemeSelect />
     </li>
        </ul>
    </div>
-</div>
-</div>
+
 </nav>
   
