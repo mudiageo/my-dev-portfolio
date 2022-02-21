5890a84fc8499eaf2bfad37ef78f8cb4f23584d9
Mudia Arha <mudiageo@gmail.com>
2022-02-21 18:44:09 +0100

    add post request handler

--- src/service-worker/fetchEvent.ts
+++ src/service-worker/fetchEvent.ts
@@ -30,8 +30,46 @@ export default (event: FetchEvent): void => {
 			})
 		);
 	}
-})
  
+
+ if (/(posts)/.test(requestURL.pathname)) {
+     const returnOfflinePosts = () => {
+       return fetch(event.request).catch(() => {
+         return caches
+           .open("postsCache")
+           .then((cache) => {
+             return cache.keys().then((cacheKeys) => {
+               return Promise.all(
+                 cacheKeys.map((cacheKey) => cache.match(cacheKey))
+               );
+             });
+           })
+           .then((cachesResponses) => {
+             return Promise.all(
+               cachesResponses.map((response) => response.json())
+             );
+           })
+           .then((posts) => {
+             const response = new Response(JSON.stringify(posts), {
+               statusText: "offline",
+             });
+             return response;
+           });
+       });
+     };
+ event.respondWith(returnOfflinePosts());
+  } else if ( /(\/posts\/)(\w+-?)*/.test(requestURL.pathname) && !/(.css)|(.js)$/.test(requestURL.pathname)) {
+     const findOfflinePost = () =>
+       caches
+         .match(request)
+         .then((response) => (response ? response : fetch(request)))
+         .catch(() => returnSSRpage("/posts/offline"));
+ event.respondWith(findOfflinePost());
+  } else
+  {
+     event.respondWith(caches.match(request).then((cacheRes) => cacheRes || fetch(request)));
+ }
+};
 /**
  * Serializes a Request into a plain JS object.
  * 
@@ -160,40 +198,3 @@ function cacheMatch(request) {
 function getPostId(request) {
 	return JSON.stringify(serializeRequest(request.clone()));
 }
- if (/(posts)/.test(requestURL.pathname)) {
-     const returnOfflinePosts = () => {
-       return fetch(event.request).catch(() => {
-         return caches
-           .open("postsCache")
-           .then((cache) => {
-             return cache.keys().then((cacheKeys) => {
-               return Promise.all(
-                 cacheKeys.map((cacheKey) => cache.match(cacheKey))
-               );
-             });
-           })
-           .then((cachesResponses) => {
-             return Promise.all(
-               cachesResponses.map((response) => response.json())
-             );
-           })
-           .then((posts) => {
-             const response = new Response(JSON.stringify(posts), {
-               statusText: "offline",
-             });
-             return response;
-           });
-       });
-     };
- event.respondWith(returnOfflinePosts());
-  } else if ( /(\/posts\/)(\w+-?)*/.test(requestURL.pathname) && !/(.css)|(.js)$/.test(requestURL.pathname)) {
-     const findOfflinePost = () =>
-       caches
-         .match(request)
-         .then((response) => (response ? response : fetch(request)))
-         .catch(() => returnSSRpage("/posts/offline"));
- event.respondWith(findOfflinePost());
-  } else
-     event.respondWith(caches.match(request).then((cacheRes) => cacheRes || fetch(request)));
- 
-};
