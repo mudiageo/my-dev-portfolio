c852bca30e2ea61304b2d3deee36aaba65dc1ced
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-02-24 14:43:21 +0100

    Create __layout.reset.svelte

---
+++ src/routes/chatbot/__layout.reset.svelte
@@ -0,0 +1,12 @@
+<script>
+import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+	import '../app.css';
+	onMount(async () => {
+	
+		themeChange(false);
+	});
+</script>
+<main>
+<slot/>
+</main>
