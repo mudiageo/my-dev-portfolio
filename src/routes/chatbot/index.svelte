ffa60a7b7102217bcd89ccab1fdb695f2eb71c57
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-02-24 15:57:02 +0100

    Update index.svelte

--- src/routes/chatbot/index.svelte
+++ src/routes/chatbot/index.svelte
@@ -11,6 +11,18 @@ const sendMessage = ()=> {
 messages = [...messages, {sender:'Mudia', message: newMessage}]
 let promptMessage = messages.map(item => `${item.sender}: ${item.message} Bot:`).toString().replaceAll('Bot:,', ' ')
 let context = `${prompt} ${promptMessage}`
+alert(context)
 }
+
+
 	
 </script>
+<form on:submit|preventDefault={sendMessage}>
+Personality: <textarea>{prompt}</textarea>
+{#each messages as {sender, message}}
+<span>{sender} : {message}</span> 
+
+{/each}
+<input type="text" bind:value={newMessage}>
+<button type="submit">Submit</button>
+</form>
