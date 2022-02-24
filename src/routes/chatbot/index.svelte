e4e449a5851961835a940b226fcf0a6a68144fe7
mudiageo <76590594+mudiageo@users.noreply.github.com>
2022-02-24 15:48:27 +0100

    Update index.svelte

--- src/routes/chatbot/index.svelte
+++ src/routes/chatbot/index.svelte
@@ -1,1 +1,16 @@
-
+<script>
+import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+	import '../app.css';
+let message = []
+let newMessage
+let prompt
+	onMount(async () => { })
+	
+const sendMessage = ()=> {
+messages = [...messages, {sender:'Mudia', message: newMessage}]
+let promptMessage = messages.map(item => `${item.sender}: ${item.message} Bot:`).toString().replaceAll('Bot:,', ' ')
+let context = `${prompt} ${promptMessage}`
+}
+	
+</script>
